3776b3fa8e69dd7ad588a4531b492f9b05afbe8a
kioni-collab <86212205+kioni-collab@users.noreply.github.com>
2022-12-01 23:47:43 -0800

    ADD: implemented add ticket and computer location
    add ticket needs some work to make sure that no ticket is added that
    doesnt have a ticket status. need to clean up some code. hope this
    doesnt get destroyed in a merge conflic
    
    Signed-off-by: kioni-collab <86212205+kioni-collab@users.noreply.github.com>

--- Backend/DBquery.py
+++ Backend/DBquery.py
@@ -1,19 +1,33 @@
 from psycopg2.extensions import cursor, AsIs
 import datetime
 
-#DOC: Returns valid building id for paramets checking
-def valid_building_id(cur:cursor):
+# DOC: Returns valid building id for paramets checking
+
+
+def valid_building_id(cur: cursor):
     cur.execute("select id from building")
     return list(cur)
 
-#DOC: Returns valid dept id for paramets checking
-def valid_dept_id(cur:cursor):
+# DOC: Returns valid dept id for paramets checking
+
+
+def valid_dept_id(cur: cursor):
     cur.execute("select id from dept")
+    return list(cur)
+
+def valid_status_id(cur: cursor):
+    cur.execute("select id from status")
+    return list(cur)
+
+def valid_asset(cur:cursor):
+    cur.execute("select barcode from asset")
     return list(cur)
 
 # DOC: Finds all the assets in a room
 # works by finding the latest tickets associeted
 # with a asset and filters by room and building
+
+
 def search_room_db(cur: cursor, room_num: str, building: int):
     cur.execute(""" select barcode,model,purch_date,type from
     (select a.*, max(t.end_date)
@@ -23,20 +37,24 @@ def search_room_db(cur: cursor, room_num: str, building: int):
     inner join room r on t.room_num = r.room_num and t.building = r.building
     inner join building b on b.id = r.building
     where r.room_num = %(room_num)s and  r.building  = %(building)s 
-    group by a.barcode) as latest""", {"room_num":room_num,"building":AsIs(building)})
+    group by a.barcode) as latest""", {"room_num": room_num, "building": AsIs(building)})
     return list(cur)
 
-#DOC: retuns all rooms in a building and the building name
+# DOC: retuns all rooms in a building and the building name
+
+
 def search_building_db(cur: cursor, building: int):
     cur.execute(""" 
     select r.room_num, b.name as building
     from building b 
     inner join room r on r.building = b.id
     where b.id = %(building)s
-    """, {"building":AsIs(building)})
+    """, {"building": AsIs(building)})
     return list(cur)
 
-#DOC: return all rooms assocaited with a department 
+# DOC: return all rooms assocaited with a department
+
+
 def search_dept_db(cur: cursor, dept: int):
     cur.execute(""" select r.room_num, b.name as building , d.name as dept
     from dept d
@@ -44,16 +62,41 @@ def search_dept_db(cur: cursor, dept: int):
     inner join room r on r.room_num = dr.room_num and r.building = dr.building
     inner join building b on r.building = b.id
     where d.id =%(dept)s
-    """, {"dept":AsIs(dept)})
+    """, {"dept": AsIs(dept)})
     return list(cur)
 
-def add_ticket_db(cur:cursor,ticket_num:int,start_date:datetime,
-end_date:datetime,room_num:str,building:int,tech_id:int,client_name:str,
-descr:str):
+
+def add_ticket_db(cur: cursor, ticket_num: int, start_date: datetime, end_date: datetime,
+                  room_num: str, building: int, tech_id: int, client_name: str,
+                  descr: str):
     cur.execute("""
     insert into Ticket(ticket_num,start_date,end_date,room_num,building,technician_id,client_name,Description)
     values (%(ticket_num)s,%(start_date)s,%(end_date)s,%(room_num)s,%(building)s, %(tech_id)s,%(cliend_name)s, %(descr)s)
-    """, {"ticket_num":ticket_num, "start_date":start_date, "end_date":end_date, "room_num":room_num,"building":building,"tech_id":tech_id,"descr":descr}
+    """, {"ticket_num": ticket_num, "start_date": start_date.strftime("%Y-%M-%D"), "end_date": end_date.strftime("%Y-%M-%D"), "room_num": room_num, "building": AsIs(building), "tech_id": AsIs(tech_id), "descr": descr}
     )
     print(cur)
 
+#todo need to figure out how to add ticket only if status is valid so all tickets have a status
+def add_ticket_asset_db(cur:cursor,ticker_num:int,barcode:int,status:int):
+    cur.execute("""
+    insert into ticket_Asset (ticket_num,barcode,status)
+    values (%(ticket_num)s,%(barcode)s,%(status)s)
+    """, {"ticket_num":ticker_num,"barcode":barcode,"status":status})
+    pass
+
+
+def search_asset_location_db(cur: cursor, barcode: int):
+    cur.execute("""
+   select room_num, building from
+        (select r.room_num as room_num, b.name as building, t.ticket_num, t.end_date
+        from asset a
+        inner join ticket_asset ta on ta.barcode = a.barcode
+        inner join ticket t on t.ticket_num = ta.ticket_num
+        inner join room r on t.room_num = r.room_num and t.building = r.building
+        inner join building b on b.id = r.building
+		 where a.barcode = %(barcode)s
+        group by r.room_num, b.name,t.ticket_num
+		order by t.end_date desc
+		limit 1) as latest
+    """, {"barcode": barcode})
+    return list(cur)
