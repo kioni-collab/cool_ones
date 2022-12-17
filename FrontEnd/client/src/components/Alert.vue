3b44442feb6f3305055c380633a20a891c8d66a3
nsayre01 <86212194+nsayre01@users.noreply.github.com>
2022-12-17 00:46:41 -0800

    fixed query variable reference

--- FrontEnd/client/src/components/Alert.vue
+++ FrontEnd/client/src/components/Alert.vue
@@ -2,7 +2,7 @@
   <div >
     <h1>{{text}}</h1>
    
-    <ticket :tickets="filteredTickets" />
+    <ticket :tickets="tickets" />
 
   </div>
 </template>
@@ -31,7 +31,7 @@ export default {
   methods: {
       async loadTickets() {
         try {
-          const tickets = await fetch("http://localhost:5000/tickethistory?room_num="+text+"&building_id=3");
+          const tickets = await fetch("http://localhost:5000/tickethistory?room_num="+this.text+"&building_id=3");
           this.tickets = await tickets.json();
           this.state = "ready";
         } catch (err) {
